58c23f1544917f006983d6c7603acb0f8cfec4c3
gijslebesque <gijslebesque@gmail.com>
2019-11-07 16:00:30 +0100

    routing ficxed

--- server/app.js
+++ server/app.js
@@ -43,7 +43,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/", function(req, res) {
+
+app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
